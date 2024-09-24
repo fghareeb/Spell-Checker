aa30cc023a39343a0c48f57c02dd83b37a6aa52e
fghareeb <134479266+fghareeb@users.noreply.github.com>
2024-09-23 22:03:42 -0400

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics; // This is added to explicitly require the javafx.graphics module
-
-    opens com.example to javafx.fxml;
-
-    exports com.example;
-}
